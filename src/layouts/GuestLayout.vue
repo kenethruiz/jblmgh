8736739b9aac4f82e0e637dac6fd0544182c68f1
kenethruiz <keneth.ruiz@yahoo.com>
2023-07-10 11:35:21 +0800

    layouts

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
@@ -9,12 +9,6 @@
           Title
         </q-toolbar-title>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
-        <q-route-tab to="/page3" label="Page Three" />
-      </q-tabs>
     </q-header>
 
     <q-page-container>
