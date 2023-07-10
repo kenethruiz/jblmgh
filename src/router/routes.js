e8c44acd534fd03e14f358135da3db27596b186f
kenethruiz <keneth.ruiz@yahoo.com>
2023-07-10 11:46:28 +0800

    pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,19 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/guest",
+    component: () => import("layouts/GuestLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/LoginPage.vue") },
+      { path: "", component: () => import("pages/RegistrationPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
